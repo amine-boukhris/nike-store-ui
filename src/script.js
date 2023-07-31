1cb5b413294ab763564f3c38f19c975e8639f5e3
amine <boukhrisamine210@gmail.com>
2023-07-31 11:16:10 +0100

    half done responsive featured section

--- src/script.js
+++ src/script.js
@@ -1,6 +1,5 @@
 // handle mobile menu visibility
 const menuBars = document.querySelector(".header__mobile-menu-icon");
-// const menuClose = document.querySelector(".mobile-menu-close-icon");
 const mobileNav = document.querySelector(".mobile-nav");
 console.log("test");
 menuBars.addEventListener("click", () => {
@@ -9,6 +8,3 @@ menuBars.addEventListener("click", () => {
     menuBars.classList.toggle("fa-xmark")
 });
 
-// menuClose.addEventListener("click", () => {
-//     mobileNav.classList.add("displaynone");
-// });
